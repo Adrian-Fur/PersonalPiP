6cb8efabd2cd366fa1cbf2bd5761bf75ec286d49
Adrian Furmanek <adrian.furm@gmail.com>
2022-02-26 15:19:00 +0100

    button styling

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('test');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+async function selectMediaStream() {
+    try {
+
+    } catch (error) {
+        alert(error);
+    }
+}
+
+//On load
+selectMediaStream();
