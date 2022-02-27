3a8211c2ecb85438ca76bfce7bf9ffb91c174dfb
Adrian Furmanek <adrian.furm@gmail.com>
2022-02-27 18:50:35 +0100

    PiP API implemented

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ const button = document.getElementById('button');
 
 async function selectMediaStream() {
     try {
-
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        } 
     } catch (error) {
         alert(error);
     }
 }
+
+button.addEventListener('click', async () => {
+    button.disabled = true;
+    await videoElement.requestPictureInPicture();
+    button.disabled = false;
+});
 
 //On load
 selectMediaStream();
